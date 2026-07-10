9131d6899acbf0a5e5c36a4f53b513085a8f7b87
Stephen Staklinski <staklins@cshl.edu>
2026-07-10 10:20:14 -0400

    Add multi-line newick tree parser function

---
+++ include/tree_parser.h
@@ -0,0 +1,23 @@
+/*
+ * VINE: Variational Inference with Node Embeddings
+ *
+ * Copyright (c) 2025-2026, Cold Spring Harbor Laboratory
+ * All rights reserved.
+ *
+ * This file is part of VINE and is distributed under the BSD 3-Clause License.
+ * See the LICENSE file in the project root for details.
+ */
+
+/* shared utilities for reading collections of Newick trees from file,
+   used by evalTrees, compareTrees, and any future tree-comparison tools */
+
+#ifndef TREE_PARSER_H
+#define TREE_PARSER_H
+
+#include <phast/lists.h>       /* List */
+
+/* Read a file of one Newick tree per line (e.g. a posterior sample) into
+ * a List of TreeNode*. */
+List *tr_read_trees_from_file(const char *fname);
+
+#endif
---
+++ src/lib/tree_parser.c
@@ -0,0 +1,43 @@
+/*
+ * VINE: Variational Inference with Node Embeddings
+ *
+ * Copyright (c) 2025-2026, Cold Spring Harbor Laboratory
+ * All rights reserved.
+ *
+ * This file is part of VINE and is distributed under the BSD 3-Clause License.
+ * See the LICENSE file in the project root for details.
+ */
+
+/* shared utilities for reading collections of Newick trees from file */
+
+#include <phast/misc.h>
+#include <phast/trees.h>
+#include <phast/stringsplus.h>
+#include "tree_parser.h"
+
+/* input file should have one newick tree 
+    per line */
+List *tr_read_trees_from_file(const char *fname) {
+  FILE *f = phast_fopen(fname, "r");
+  String *line = str_new(STR_VERY_LONG_LEN);
+  /* set initial capacity that is auto allocated larger as needed */
+  List *trees = lst_new_ptr(1000);
+  int lineno = 0; /* count of trees */
+  while (str_readline(line, f) != EOF) {
+    str_double_trim(line);  /* remove whitespace */
+    if (line->length == 0) continue;  /* skip empty lines */
+    lineno++;
+    if (line->chars[0] != '(')
+      die("ERROR in line %d of %s: Input does not look like a Newick-formatted tree.\n",
+          lineno, fname);
+      
+    /* remove trailing semicolon if present since
+        tr_new_from_string() expects a string without it */
+    if (line->chars[line->length-1] == ';')
+      line->chars[--line->length] = '\0';
+    lst_push_ptr(trees, tr_new_from_string(line->chars));
+  }
+  str_free(line);
+  fclose(f);
+  return trees;
+}
