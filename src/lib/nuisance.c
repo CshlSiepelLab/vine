470cb03046ad5f0db4e711f8d3d521e5f07ec0d8
Stephen Staklinski <staklins@cshl.edu>
2026-07-13 14:53:00 -0400

    Add the migration rate prior gradient to the rate nuisance parameters

--- src/lib/nuisance.c
+++ src/lib/nuisance.c
@@ -217,8 +217,13 @@ void update_nuis_grad(TreeModel *mod, CovarData *data, Vector *nuis_grad) {
   if (data->migtable != NULL) {
     /* zero migration gradients during warmup phase */
     double mig_scale = (data->crispr_mod != NULL && data->crispr_mod->mig_warmup) ? 0.0 : 1.0;
-    for (i = 0; i < data->migtable->deriv_gtr->size; i++)
-      vec_set(nuis_grad, idx++, mig_scale * vec_get(data->migtable->deriv_gtr, i));
+    for (i = 0; i < data->migtable->deriv_gtr->size; i++) {
+      double mig_grad = vec_get(data->migtable->deriv_gtr, i);
+      if (data->migtable->use_rate_prior)
+        /* exponential(1) prior has derivative -1 */
+        mig_grad -= 1.0;
+      vec_set(nuis_grad, idx++, mig_scale * mig_grad);
+    }
   }
 
   assert(idx == nuis_grad->size);
